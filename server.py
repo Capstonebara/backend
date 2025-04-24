233e99e64cb3d4bdafdaf87174c2d31bf711ee57
thainq <thainguyen12.work@gmail.com>
2025-04-24 18:41:57 +0700

    add timestamp log + using gunicorn as production

--- server.py
+++ server.py
@@ -2,3 +2,6 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=5500, reload=True, timeout_keep_alive=120)
+
+# To run the server, use the command:
+# gunicorn main:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:5500 --workers 4 --timeout 120 --log-config logging.ini
